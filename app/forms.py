ac4e208064dee1221e8accd207e2a74cc32ef001
Andrei Vacariu <andrei@avacariu.me>
2014-08-15 19:30:52 -0700

    Made it look better.

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,16 @@
 from flask.ext.wtf import Form
 from wtforms import TextField, HiddenField, IntegerField, BooleanField
-from wtforms.validators import Required
+from wtforms.validators import Required, ValidationError
+
+def positive(form, field):
+    if field.data <= 0:
+        raise ValidationError('Field must be positive')
 
 class NewThingForm(Form):
     name = TextField('name', validators = [Required()])
-    delta = IntegerField('delta', validators = [Required()])
+    delta = IntegerField('delta', validators = [Required(), positive])
 
 class EditThingForm(Form):
     name = TextField('name', validators = [Required()])
-    delta = IntegerField('delta', validators = [Required()])
+    delta = IntegerField('delta', validators = [Required(), positive])
     delete = BooleanField('delete')
