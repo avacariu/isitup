62daafde7d90f06807e0bc407c20a1b6c5f02627
Andrei Vacariu <andrei@avacariu.me>
2016-07-25 11:29:35 -0700

    Clean up forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,16 +1,19 @@
 from flask_wtf import Form
-from wtforms import TextField, HiddenField, IntegerField, BooleanField
+from wtforms import TextField, IntegerField, BooleanField
 from wtforms.validators import Required, ValidationError
+
 
 def positive(form, field):
     if field.data <= 0:
         raise ValidationError('Field must be positive')
 
+
 class NewThingForm(Form):
-    name = TextField('name', validators = [Required()])
-    delta = IntegerField('delta', validators = [Required(), positive])
+    name = TextField('name', validators=[Required()])
+    delta = IntegerField('delta', validators=[Required(), positive])
+
 
 class EditThingForm(Form):
-    name = TextField('name', validators = [Required()])
-    delta = IntegerField('delta', validators = [Required(), positive])
+    name = TextField('name', validators=[Required()])
+    delta = IntegerField('delta', validators=[Required(), positive])
     delete = BooleanField('delete')
